31107960077657447ac348e5a57340cf5b163321
David Buchmann <david@liip.ch>
2016-02-26 19:30:18 +0100

    validate type specific options

--- Tests/Resources/Fixtures/config/full.php
+++ Tests/Resources/Fixtures/config/full.php
@@ -13,4 +13,58 @@ $container->loadFromExtension('httplug', [
         'uri_factory'     => 'Http\Message\UriFactory\GuzzleUriFactory',
         'stream_factory'  => 'Http\Message\StreamFactory\GuzzleStreamFactory',
     ],
+    'toolbar' => [
+        'enabled' => true,
+        'formatter' => 'my_toolbar_formatter',
+    ],
+    'plugins' => [
+        'authentication' => [
+            'my_basic' => [
+                'type' => 'basic',
+                'username' => 'foo',
+                'password' => 'bar',
+            ],
+            'my_wsse' => [
+                'type' => 'wsse',
+                'username' => 'foo',
+                'password' => 'bar',
+            ],
+            'my_brearer' => [
+                'type' => 'bearer',
+                'token' => 'foo',
+            ],
+            'my_service' => [
+                'type' => 'service',
+                'service' => 'my_auth_serivce',
+            ],
+        ],
+        'cache' => [
+            'stream_factory' => 'my_other_stream_factory',
+            'config' => [
+                'default_ttl' => 42,
+                'respect_cache_headers' => false,
+            ],
+        ],
+        'cookie' => [
+            'cookie_jar' => 'my_cookie_jar',
+        ],
+        'decoder' => [
+            'enabled' => false,
+        ],
+        'history' => [
+            'journal' => 'my_journal',
+        ],
+        'logger' => [
+            'enabled' => false,
+        ],
+        'redirect' => [
+            'enabled' => false,
+        ],
+        'retry' => [
+            'enabled' => false,
+        ],
+        'stopwatch' => [
+            'enabled' => false,
+        ],
+    ],
 ]);
