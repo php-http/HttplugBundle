be6f9f51088a48bcfca974c3d964ca57aa37385d
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-06-30 14:19:09 +0200

    Allow CURL constant names (#82)
    Allow CURL constant names and validate we found them

--- ClientFactory/CurlFactory.php
+++ ClientFactory/CurlFactory.php
@@ -40,6 +40,19 @@ class CurlFactory implements ClientFactory
             throw new \LogicException('To use the Curl client you need to install the "php-http/curl-client" package.');
         }
 
+        // Try to resolve curl constant names
+        foreach ($config as $key => $value) {
+            // If the $key is a string we assume it is a constant
+            if (is_string($key)) {
+                if (null === ($constantValue = constant($key))) {
+                    throw new \LogicException(sprintf('Key %s is not an int nor a CURL constant', $key));
+                }
+
+                unset($config[$key]);
+                $config[$constantValue] = $value;
+            }
+        }
+
         return new Client($this->messageFactory, $this->streamFactory, $config);
     }
 }
