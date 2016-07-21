119f8875843c24c80b57593ef1469c768e95d5f1
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-07-21 17:32:39 +0200

    Dev tests (#98)
    More tests for the DI extension
    
    * Added test for discovery strategy
    * Added test for no-debug
    * Test if profiling is enabled when it should be

--- Tests/Unit/DependencyInjection/HttplugExtensionTest.php
+++ Tests/Unit/DependencyInjection/HttplugExtensionTest.php
@@ -7,6 +7,7 @@ use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
 
 /**
  * @author David Buchmann <mail@davidbu.ch>
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
 class HttplugExtensionTest extends AbstractExtensionTestCase
 {
@@ -50,6 +51,80 @@ class HttplugExtensionTest extends AbstractExtensionTestCase
 
         foreach (['client', 'message_factory', 'uri_factory', 'stream_factory'] as $type) {
             $this->assertContainerBuilderHasAlias("httplug.$type", "my_{$type}_service");
+        }
+    }
+
+    public function testNoProfilingWhenToolbarIsDisabled()
+    {
+        $this->load(
+            [
+                'toolbar' => [
+                    'enabled' => false,
+                ],
+                'clients' => [
+                    'acme' => [
+                        'factory' => 'httplug.factory.curl',
+                        'plugins' => ['foo'],
+                    ],
+                ],
+            ]
+        );
+
+        $this->verifyProfilingDisabled();
+    }
+
+    public function testNoProfilingWhenNotInDebugMode()
+    {
+        $this->setParameter('kernel.debug', false);
+        $this->load(
+            [
+                'clients' => [
+                    'acme' => [
+                        'factory' => 'httplug.factory.curl',
+                        'plugins' => ['foo'],
+                    ],
+                ],
+            ]
+        );
+
+        $this->verifyProfilingDisabled();
+    }
+
+    public function testProfilingWhenToolbarIsSpecificallyOn()
+    {
+        $this->setParameter('kernel.debug', false);
+        $this->load(
+            [
+                'toolbar' => [
+                    'enabled' => true,
+                ],
+                'clients' => [
+                    'acme' => [
+                        'factory' => 'httplug.factory.curl',
+                        'plugins' => ['foo'],
+                    ],
+                ],
+            ]
+        );
+
+        $def = $this->container->findDefinition('httplug.client');
+        $arguments = $def->getArguments();
+
+        $this->assertTrue(isset($arguments[3]));
+        $this->assertTrue(isset($arguments[3]['debug_plugins']));
+        $this->assertFalse(empty($arguments[3]['debug_plugins']));
+    }
+
+    private function verifyProfilingDisabled()
+    {
+        $def = $this->container->findDefinition('httplug.client');
+        $arguments = $def->getArguments();
+
+        if (isset($arguments[3])) {
+            $this->assertEmpty(
+                $arguments[3],
+                'Parameter 3 to the PluginClient must not contain any debug_plugin information when profiling is disabled'
+            );
         }
     }
 }
---
+++ Tests/Unit/Discovery/ConfiguredClientsStrategyTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Http\HttplugBundle\Tests\Unit\Discovery;
+
+use Http\Client\HttpAsyncClient;
+use Http\Client\HttpClient;
+use Http\HttplugBundle\Discovery\ConfiguredClientsStrategy;
+
+class ConfiguredClientsStrategyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetCandidates()
+    {
+        $httpClient = $this->getMock(HttpClient::class);
+        $httpAsyncClient = $this->getMock(HttpAsyncClient::class);
+        $strategy = new ConfiguredClientsStrategy($httpClient, $httpAsyncClient);
+
+        $candidates = $strategy::getCandidates(HttpClient::class);
+        $candidate = array_shift($candidates);
+        $this->assertEquals($httpClient, $candidate['class']());
+
+        $candidates = $strategy::getCandidates(HttpAsyncClient::class);
+        $candidate = array_shift($candidates);
+        $this->assertEquals($httpAsyncClient, $candidate['class']());
+    }
+
+    public function testGetCandidatesEmpty()
+    {
+        $strategy = new ConfiguredClientsStrategy(null, null);
+
+        $candidates = $strategy::getCandidates(HttpClient::class);
+        $this->assertEquals([], $candidates);
+
+        $candidates = $strategy::getCandidates(HttpAsyncClient::class);
+        $this->assertEquals([], $candidates);
+    }
+
+    public function testGetCandidatesEmptyAsync()
+    {
+        $httpClient = $this->getMock(HttpClient::class);
+        $strategy = new ConfiguredClientsStrategy($httpClient, null);
+
+        $candidates = $strategy::getCandidates(HttpClient::class);
+        $candidate = array_shift($candidates);
+        $this->assertEquals($httpClient, $candidate['class']());
+
+        $candidates = $strategy::getCandidates(HttpAsyncClient::class);
+        $this->assertEquals([], $candidates);
+    }
+
+    public function testGetCandidatesEmptySync()
+    {
+        $httpAsyncClient = $this->getMock(HttpAsyncClient::class);
+        $strategy = new ConfiguredClientsStrategy(null, $httpAsyncClient);
+
+        $candidates = $strategy::getCandidates(HttpClient::class);
+        $this->assertEquals([], $candidates);
+
+        $candidates = $strategy::getCandidates(HttpAsyncClient::class);
+        $candidate = array_shift($candidates);
+        $this->assertEquals($httpAsyncClient, $candidate['class']());
+    }
+}
