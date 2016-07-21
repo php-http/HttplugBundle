9b89ecf66e4f1c5a7972f158c8fa9381c281056d
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-07-21 16:18:41 +0200

    Added test for no-debug

--- Tests/Functional/ServiceInstantiationTest.php
+++ Tests/Functional/ServiceInstantiationTest.php
@@ -6,13 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ServiceInstantiationTest extends WebTestCase
 {
-    public static function setUpBeforeClass()
-    {
-        static::bootKernel();
-    }
-
     public function testHttpClient()
     {
+        static::bootKernel();
+        $container = static::$kernel->getContainer();
+        $this->assertTrue($container->has('httplug.client'));
+        $client = $container->get('httplug.client');
+        $this->assertInstanceOf('Http\Client\HttpClient', $client);
+    }
+
+    public function testHttpClientNoDebug()
+    {
+        static::bootKernel(['debug'=>false]);
         $container = static::$kernel->getContainer();
         $this->assertTrue($container->has('httplug.client'));
         $client = $container->get('httplug.client');
