167fc5f60c957c7dbb765aa4ae43dc12721d1f22
Михаил Красильников <m.krasilnikov@yandex.ru>
2016-09-12 21:09:53 +0300

    DummyClient should provide methods of HttpClient and HttpAsyncClient
    This allow static analysis and code autocomplete in IDEs.

--- ClientFactory/DummyClient.php
+++ ClientFactory/DummyClient.php
@@ -2,11 +2,14 @@
 
 namespace Http\HttplugBundle\ClientFactory;
 
+use Http\Client\HttpAsyncClient;
+use Http\Client\HttpClient;
+
 /**
  * This client is used as a placeholder for the dependency injection. It will never be used.
  *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
-class DummyClient
+interface DummyClient extends HttpClient, HttpAsyncClient
 {
 }
