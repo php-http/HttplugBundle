68f858f07d82848aa75e67fa0367739fa9d44293
Orest Divintari <o.divintari@gmail.com>
2025-12-19 22:25:59 +0200

    fix code style

--- tests/Unit/DependencyInjection/ConfigurationTest.php
+++ tests/Unit/DependencyInjection/ConfigurationTest.php
@@ -13,8 +13,6 @@ use Nyholm\Psr7\Factory\Psr17Factory;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\HttpKernel\Kernel;
-use function class_exists;
 
 /**
  * @author David Buchmann <mail@davidbu.ch>
@@ -117,7 +115,7 @@ final class ConfigurationTest extends AbstractExtensionConfigurationTestCase
 
     public function testSupportsAllConfigFormats(): void
     {
-        if (!class_exists(Client::class)) {
+        if (!\class_exists(Client::class)) {
             $this->markTestSkipped('Guzzle 7 adapter is not installed');
         }
 
@@ -330,7 +328,7 @@ final class ConfigurationTest extends AbstractExtensionConfigurationTestCase
         ];
 
         // XML configuration is not supported in Symfony 8+
-        if (class_exists('Symfony\Component\DependencyInjection\Loader\XmlFileLoader')) {
+        if (\class_exists('Symfony\Component\DependencyInjection\Loader\XmlFileLoader')) {
             $formats[] = 'config/full.xml';
         }
 
